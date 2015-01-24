bac7f22d4979943616243560daf3a051557159fc
Tejas Manohar <me@tejasmanohar.com>
2015-01-24 11:31:08 -0600

    base jquery

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var request = require('request');
 request('http://www.kayak.com/flight-routes/United-States-US0/Nashville-Intl-BNA', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    console.log('lel')
     console.log($('.results_price bookitprice').length);
   }
 });
